bb2730df7962ec578eeec6424ccd9c0e7a591115
jay <p0425@nate.com>
2023-12-18 17:55:19 +0900

    1. Apollo Codegen -> graphql codegen 사용 2. UI Clonning

---
+++ codegen.ts
@@ -0,0 +1,12 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "http://localhost:4000/graphql",
+  documents: ["src/**/*.tsx"],
+  generates: {
+    "./src/__generated__/": {
+      preset: "client",
+    },
+  },
+};
+export default config;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,100 @@
+import { useForm } from "react-hook-form";
+import { FormError } from "../components/form-error";
+import { gql, useMutation } from "@apollo/client";
+import nuberLogo from "../images/logo.svg";
+import {
+  LoginMutation,
+  LoginMutationVariables,
+} from "../__generated__/graphql";
+
+// mutation 이름은 backend로 넘어가지 않고
+// frontend에서만 사용된다.
+const LOGIN_MUTATION = gql`
+  mutation login($loginInput: LoginInput!) {
+    login(input: $loginInput) {
+      ok
+      token
+      error
+    }
+  }
+`;
+
+interface ILoginForm {
+  email: string;
+  password: string;
+  resultError?: string;
+}
+
 export const Login = () => {
+  const {
+    register,
+    getValues,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<ILoginForm>();
+  const onCompleted = (data: LoginMutation) => {
+    const {
+      login: { ok, token },
+    } = data;
+    if (ok) {
+      console.log(token);
+    }
+  };
+  const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
+    LoginMutation,
+    LoginMutationVariables
+  >(LOGIN_MUTATION, {
+    onCompleted,
+  });
+  const onSubmit = () => {
+    if (!loading) {
+      const { email, password } = getValues();
+      loginMutation({
+        variables: {
+          loginInput: {
+            email,
+            password,
+          },
+        },
+      });
+    }
+  };
+
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
-        <h3 className="text-2xl text-gray-800">Log In</h3>
-        <form className="flex flex-col mt-5 px-5">
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={nuberLogo} className="w-52 mb-10" />
+        <h4 className="w-full font-medium text-left text-3xl mb-5">
+          Welcome back
+        </h4>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 w-full"
+        >
           <input
+            {...register("email", { required: "Email is required" })}
+            type="email"
             placeholder="Email"
-            className=" bg-gray-100 shadow-inner border-2 focus:border-opacity-60 focus:border-green-600 focus:outline-none mb-3 py-3 px-5 rounded-lg"
+            className="input"
           />
+          {errors.email?.message && (
+            <FormError errorMessage={errors.email?.message} />
+          )}
           <input
+            {...register("password", {
+              required: "Password is required",
+            })}
+            type="password"
             placeholder="Password"
-            className=" bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-60 focus:border-green-600 py-3 px-5 rounded-lg"
+            className="input"
           />
-          <button className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90">
-            Log In
-          </button>
+          {errors.password?.message && (
+            <FormError errorMessage={errors.password?.message} />
+          )}
+          <button className="btn">{loading ? "Loading..." : "Log In"}</button>
+          {loginMutationResult?.login.error && (
+            <FormError errorMessage={loginMutationResult.login.error} />
+          )}
         </form>
       </div>
     </div>
